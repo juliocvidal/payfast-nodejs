208398bdfe152f21129aef4df5ba87182e4b7b97
juliocvidal <juliocvidal@gmail.com>
2016-04-11 09:45:37 -0300

    [cap.1][introducao] express-load

--- config/express.js
+++ config/express.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var load = require('express-load');
 
 module.exports = function() {
 
     var app = express();
+
+     load('controllers', {cwd: 'app'})
+      .into(app);
 
     return app;
 
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var app = require('./config/express')();
 
-app.get('/hello', function (req, res) {
-   res.send('hi');
-});
-
 app.listen(3000, function(){
   console.log("Server up!");
 });
