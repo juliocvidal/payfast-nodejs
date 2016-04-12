b28aff8c614196e40cdfc3fce139597e439fa6ab
juliocvidal <juliocvidal@gmail.com>
2016-04-12 18:20:29 -0300

    [cap.2][controllers] mudar status e salvar no banco

--- app/controllers/pagamentos.js
+++ app/controllers/pagamentos.js
@@ -6,8 +6,15 @@ module.exports = function(app) {
 
     app.post("/pagamentos/pagamento",function(req, res) {
       var pagamento = req.body;
+      console.log('processando pagamento...');
 
-      console.log(pagamento);
-      res.send('ok');
+      var connection = app.infra.connectionFactory();
+      var pagamentoDao = new app.infra.PagamentoDao(connection);
+
+      pagamento.status = "CRIADO";
+      pagamento.data =  new Date;
+      pagamentoDao.salva(pagamento);
+
+      res.json(pagamento);
     });
 }
--- config/express.js
+++ config/express.js
@@ -3,15 +3,13 @@ var load = require('express-load');
 var bodyParser = require('body-parser');
 
 module.exports = function() {
-
   var app = express();
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
   load('controllers', {cwd: 'app'})
+      .then('infra')
       .into(app);
-
   return app;
-
 }
