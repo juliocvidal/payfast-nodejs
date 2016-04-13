b6792c24570abbd2c081c79ed6ce30c7fb4473b1
juliocvidal <juliocvidal@gmail.com>
2016-04-13 19:34:02 -0300

    [cap.2][controllers] validacao

--- app/controllers/pagamentos.js
+++ app/controllers/pagamentos.js
@@ -1,6 +1,20 @@
 module.exports = function(app) {
     app.post("/pagamentos/pagamento",function(req, res) {
       var pagamento = req.body;
+
+      req.assert("forma_de_pagamento", "Forma de pagamento é obrigatória.").notEmpty();
+      req.assert("valor", "Valor é obrigatório e deve ser um decimal.").notEmpty().isFloat();
+      req.assert("moeda", "Moeda é obrigatória e deve ter 3 caracteres").notEmpty().len(3,3);
+
+      var errors = req.validationErrors();
+
+      if (errors){
+        console.log("Erros de validação encontrados");
+
+        res.status(400).send(errors);
+        return;
+      }
+
       console.log('processando pagamento...');
 
       var connection = app.infra.connectionFactory();
--- config/express.js
+++ config/express.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function() {
   var app = express();
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+
+  app.use(expressValidator()); //Obrigatoriamente logo apos o bodyParser
 
   load('controllers', {cwd: 'app'})
       .then('infra')
