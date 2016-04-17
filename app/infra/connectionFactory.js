358f241619fa1ebecf3a7ec8ee067b36d0b39458
juliocvidal <juliocvidal@gmail.com>
2016-04-17 17:35:21 -0300

    [cap.3][servicos] sandbox

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,12 +1,23 @@
 var mysql  = require('mysql');
 
 function createDBConnection(){
+	if (!process.env.NODE_ENV) {
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
 			password: '',
 			database: 'payfast'
 		});
+	}
+
+	if (process.env.NODE_ENV == 'sandbox') {
+		return mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'payfast_sandbox'
+		});
+	}
 }
 
 module.exports = function() {
