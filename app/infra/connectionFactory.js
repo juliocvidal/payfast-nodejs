0fb010ff8361f0b0a93c2e04ff3d336fa5b99d99
juliocvidal <juliocvidal@gmail.com>
2016-04-18 04:10:43 -0300

    [cap.8][packaging] deploy

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,7 +1,7 @@
 var mysql  = require('mysql');
 
 function createDBConnection(){
-	if (!process.env.NODE_ENV) {
+	if (!process.env.NODE_ENV || process.env.node === 'dev') {
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
@@ -18,6 +18,17 @@ function createDBConnection(){
 			database: 'payfast_sandbox'
 		});
 	}
+
+	if (process.env.NODE_ENV == 'production') {
+	 var url = process.env.CLEARDB_DATABASE_URL;
+	 var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+	 return mysql.createConnection({
+		 host:grupos[3],
+		 user:grupos[1],
+		 password:grupos[2],
+		 database:grupos[4]
+	 });
+ }
 }
 
 module.exports = function() {
