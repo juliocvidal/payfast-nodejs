e15f46b0859731f32f11b2afeae541bf04d9c213
juliocvidal <juliocvidal@gmail.com>
2016-04-18 04:38:45 -0300

    [cap.9][escalabilidade] cluster

--- config/express.js
+++ config/express.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var morgan = require('morgan');
 var logger = require('../app/infra/logger.js');
-var core = require('core');
+var cors = require('cors');
 
 module.exports = function() {
   var app = express();
@@ -17,9 +17,9 @@ module.exports = function() {
     }
   }));
 
-  app.use('cors'({
-    origin: "http://localhost:3001";
-    methods: ["GET". "POST", "PUT", "DELETE"],
+  app.use(cors({
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: "Content-type"
   }));
 
